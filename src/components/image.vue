d77d151b463d04aa61fda6a6dd96f357c81fa81e
xiyue <xiyuesaves@gmail.com>
2021-11-23 10:31:36 +0800

    新增图片查看组件

---
+++ src/components/image.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="img-view">
+    <img :src="`http://127.0.0.1:88/raw${Dwindow.location.pathname}${selectFile}`">
+  </div>
+</template>
+<script>
+export default {
+  props: ["selectFile"],
+  data() {
+    return {
+      Dwindow: window
+    }
+  }
+}
+
+</script>
+<style scoped>
+.img-view {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+</style>
