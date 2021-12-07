70c4b8cdab861c28df0216544a5a34eca1713b0e
xiyue <xiyuesaves@gmail.com>
2021-12-07 11:34:44 +0800

    增加预览窗口隐藏侧边栏

--- src/components/previewFile.vue
+++ src/components/previewFile.vue
@@ -8,12 +8,13 @@
       </div>
     </div>
     <div class="content">
-      <div class="preview-view">
+      <div :class="['preview-view',{'hidenItem':!showItem}]">
         <download v-if="showPreviewPage(selectFile ? selectFile.split('.').pop() : '') === 'defaultFile'" :selectFile="selectFile" />
         <images v-if="showPreviewPage(selectFile ? selectFile.split('.').pop() : '') === 'image'" :selectFile="selectFile" />
         <viewText v-if="showPreviewPage(selectFile ? selectFile.split('.').pop() : '') === 'text'" :selectFile="selectFile" />
       </div>
-      <div class="right-item">
+      <div :class="['right-item',{'hidenItem':!showItem}]">
+        <div class="hidden-item" :title="showItem ? '隐藏侧边栏' : '展开侧边栏'" @click="hidenRightItem"></div>
         <div ref="fileListEl" @scroll="handleScroll" class="scroll-file-list">
           <div :style="`height: ${scrollH}px;`" class="scroll-box">
             <div @click="selectOther(file.name)" :style="`transform: translateY(${offsetY}px);`" v-for="(file,idnex) in realRender" v-if="file.type !== 'floder'" :class="['file-item',{'active':file.name === selectFile}]">
@@ -54,9 +55,14 @@ export default {
       showNum: 0,
       loadingView: true,
       source: this.axios.CancelToken.source(),
+      showItem: true
     }
   },
   methods: {
+    hidenRightItem() {
+      console.log("隐藏侧边栏")
+      this.showItem = !this.showItem
+    },
     filterFile(arr, type) {
       let newArr = []
       for (var i = 0; i < arr.length; i++) {
@@ -210,6 +216,11 @@ export default {
   height: 100%;
   /*padding: 16px;*/
   position: relative;
+  transition: width 300ms;
+}
+
+.content .preview-view.hidenItem{
+  width: 100%;
 }
 
 .content .loading-view {
@@ -231,8 +242,15 @@ export default {
   height: 100%;
   width: 296px;
   /*padding: 16px;*/
-  overflow: hidden;
+  /*overflow: hidden;*/
+  position: relative;
   border-left: solid 1px #d5d8da;
+  transition: width 300ms;
+}
+
+.content .right-item.hidenItem{
+  width: 0px;
+  border-left: none;
 }
 
 .content .right-item .scroll-file-list {
@@ -254,6 +272,68 @@ export default {
   background-color: #d7dadd;
 }
 
+.content .right-item.hidenItem .hidden-item{
+  /*opacity: 1;*/
+  pointer-events: auto;
+  transform: rotateY(180deg);
+  left: -10px;
+}
+
+.content .right-item.hidenItem .hidden-item:hover{
+  opacity: 1;
+}
+
+.content .right-item .hidden-item {
+  position: absolute;
+  width: 10px;
+  height: 60px;
+  top: calc(50% - 60px);
+  left: 0;
+  z-index: 10;
+  background-color: #f8f8f8;
+  cursor: pointer;
+  opacity: 0;
+  pointer-events: none;
+  /*transform: rotateY(180deg);*/
+  /*left: -10px;*/
+  transition: opacity 150ms;
+}
+
+.content .right-item:hover .hidden-item {
+  pointer-events: auto;
+  opacity: 1;
+}
+
+.content .right-item .hidden-item::after {
+  content: "";
+  position: absolute;
+  display: block;
+  top: 0;
+  left: 0;
+  width: 10px;
+  height: 60px;
+  background-color: #f8f8f8;
+  box-shadow: 2px 0 4px rgba(0, 0, 0, 0.08);
+  border: solid 1px #dfdfdf;
+  border-left: none;
+  border-right: solid 2px #dfdfdf;
+  border-radius: 0 4px 4px 0;
+  transform: perspective(10px) rotateY(10deg);
+}
+
+.content .right-item .hidden-item::before {
+  content: "";
+  position: absolute;
+  display: block;
+  z-index: 20;
+  width: 8px;
+  left: -4px;
+  top: calc(50% - 4px);
+  box-shadow: 2px -2px 0 #858D93A1;
+  transform: rotateZ(45deg);
+  height: 8px;
+  /*background-color: red;*/
+}
 
 .scroll-box {
   width: 100%;
@@ -330,6 +410,16 @@ export default {
   text-align: right;
   box-sizing: border-box;
   color: #666666;
+}
+
+@media (max-width: 820px) {
+  .right-item {
+    display: none;
+  }
+
+  .content .preview-view {
+    width: 100%;
+  }
 }
 
 </style>
