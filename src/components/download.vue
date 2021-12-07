f485635c5eb587dad323a92107f24b6db0ccb7c7
xiyue <xiyuesaves@gmail.com>
2021-12-07 15:56:51 +0800

    下载页面图标将跟随文件类型

--- src/components/download.vue
+++ src/components/download.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="download">
-    <span class="iconfont file-icon icon-wenjian"></span>
+    <span :class="['iconfont','file-icon',fileIcons(selectFile.split('.').pop())]"></span>
     <p class="selFile">{{selectFile}}</p>
     <menuButton title="下载该文件" showText="下载" :clickFun="downloadThisFile" />
   </div>
@@ -8,7 +8,7 @@
 <script>
 import menuButton from "./menuButton"
 export default {
-  props: ["selectFile"],
+  props: ["selectFile","fileIcons"],
   data() {
     return {}
   },
