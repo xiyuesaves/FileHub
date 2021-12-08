e2de6835ddb53a23c259533ea417a8fdb5ebc3b1
xiyue <xiyuesaves@gmail.com>
2021-12-08 15:32:29 +0800

    移除highlight.js 修复json文件被错误解析

--- src/components/viewText.vue
+++ src/components/viewText.vue
@@ -3,21 +3,22 @@
   </pre>
 </template>
 <script>
-import hljs from 'highlight.js'
 export default {
   props: ["selectFile"],
   data() {
     return {
-      showText: ""
+      showText: "",
+      localhost: this.$parent.$parent.$parent.localhost
     }
   },
   methods: {
     getRaw() {
-      this.axios.get(`http://127.0.0.1:88/raw${window.location.pathname}${this.selectFile}`,{
-      	// 阻止axios自动格式化json
-      	transformResponse: (res) => {
-            return res;
-        }}).then(res => {
+      this.axios.get(`${this.localhost}/raw${window.location.pathname}${this.selectFile}`, {
+        // 阻止axios自动格式化json
+        transformResponse: (res) => {
+          return res;
+        }
+      }).then(res => {
         this.showText = res.data
       })
     }
@@ -34,35 +35,40 @@ export default {
 
 </script>
 <style scoped>
-	.text-view{
-		font-family: "Consolas","微软雅黑";
-		font-size: 16px;
-    box-sizing: border-box;
-    line-height: 22px;
-		padding: 8px;
-		margin: 0;
-		width: 100%;
-		height: 100%;
-		overflow: auto;
-	}
-  .text-view::-webkit-scrollbar-button{
-    display: none;
-  }
-  .text-view::-webkit-scrollbar-corner,
-  .text-view::-webkit-scrollbar{
-    width: 7px;
-    height: 7px;
-    background-color: #f1f1f1;
-  }
-  .text-view:hover:-webkit-scrollbar{
-    width: 8px;
-  }
-  .text-view::-webkit-scrollbar-thumb{
-    background-color: #d7dadd;
-  }
-  /*.text-view::-webkit-scrollbar-corner,
-  .text-view::-webkit-scrollbar-track-piece{
-    background-color: #dddddd;
-  }*/
+.text-view {
+  font-family: "Consolas", "微软雅黑";
+  font-size: 16px;
+  box-sizing: border-box;
+  line-height: 22px;
+  padding: 8px;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+}
+
+.text-view::-webkit-scrollbar-button {
+  display: none;
+}
+
+.text-view::-webkit-scrollbar-corner,
+.text-view::-webkit-scrollbar {
+  width: 7px;
+  height: 7px;
+  background-color: #f1f1f1;
+}
+
+.text-view:hover:-webkit-scrollbar {
+  width: 8px;
+}
+
+.text-view::-webkit-scrollbar-thumb {
+  background-color: #d7dadd;
+}
+
+.text-view::selection {
+  background: #0969da;
+  color: #ffffff;
+}
 
 </style>
